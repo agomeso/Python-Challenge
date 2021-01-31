165b4551436086b871385af7948835e3dab303db
agomeso <alejandrogomezmba@gmail.com>
2021-01-30 22:45:51 -0600

    Getting set-up

---
+++ PyPoll/main.py
@@ -0,0 +1,16 @@
+# The total number of votes cast
+# A complete list of candidates who received votes
+# The percentage of votes each candidate won
+# The total number of votes each candidate won
+# The winner of the election based on popular vote.
+print("Election Results")
+print("-------------------------")
+print("Total Votes: 3521001")
+print("-------------------------")
+print("Khan: 63.000 % (2218231)")
+print("Correy: 20.000 % (704200)")
+print("Li: 14.000 % (492940)")
+print("O'Tooley: 3.000 % (105630)")
+print("-------------------------")
+print("Winner: Khan")
+print("-------------------------")
