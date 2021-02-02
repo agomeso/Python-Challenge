190c3f59c3bf1c76a4424509823fc2b9a272743d
agomeso <alejandrogomezmba@gmail.com>
2021-02-01 21:26:00 -0600

    Output

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,7 +3,7 @@ file_path = "./Resources/election_data.csv"
 total_votes = 0
 election = {"Voter ID": "", "County": "", "Candidate": ""}
 got_votes = {}
-
+output_file = "./Analysis/output.txt"
 with open(file_path) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile)
@@ -36,18 +36,37 @@ for key, value in got_votes.items():
 # print(elected)
 # get rid of the ' on O'Tooley
 o = str("O'Tooley")
+# print all the results
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {total_votes}")
 print("-------------------------")
 print(
-    f"Khan: {str(round(got_votes['Khan']/total_votes*100,5))}% ({got_votes['Khan']})")
+    f"Khan: {round(got_votes['Khan']/total_votes*100,3)}% ({got_votes['Khan']})")
 print(
-    f"Correy: {round(float(got_votes['Correy']/total_votes*100),5)}% ({got_votes['Correy']})")
+    f"Correy: {round(got_votes['Correy']/total_votes*100,3)}% ({got_votes['Correy']})")
 print(
-    f"Li: {round(float(got_votes['Li']/total_votes*100),5)}% ({got_votes['Li']})")
+    f"Li: {round(got_votes['Li']/total_votes*100,3)}% ({got_votes['Li']})")
 print(
-    f"O'Tooley: {round(float(got_votes[o]/total_votes*100),5)}% ({got_votes[o]})")
+    f"O'Tooley: {round(got_votes[o]/total_votes*100,3)}% ({got_votes[o]})")
 print("-------------------------")
 print(f"Winner: {elected}")
 print("-------------------------")
+
+# output
+with open(output_file, "w") as outputFile:
+    outputFile.write("Election Results\n")
+    outputFile.write("----------------------------\n")
+    outputFile.write(f"Total Votes: {total_votes}\n")
+    outputFile.write("----------------------------\n")
+    outputFile.write(
+        f"Khan: {round(got_votes['Khan']/total_votes*100,3)}% ({got_votes['Khan']})\n")
+    outputFile.write(
+        f"Correy: {round(got_votes['Correy']/total_votes*100,3)}% ({got_votes['Correy']})\n")
+    outputFile.write(
+        f"Li: {round(got_votes['Li']/total_votes*100,3)}% ({got_votes['Li']})\n")
+    outputFile.write(
+        f"O'Tooley: {round(got_votes[o]/total_votes*100,3)}% ({got_votes[o]})\n")
+    outputFile.write("----------------------------\n")
+    outputFile.write(f"Winner: {elected}\n")
+    outputFile.write("----------------------------\n")
