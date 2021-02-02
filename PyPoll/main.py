ca00f87523fe4ad87c2b9398af816723d191182c
agomeso <alejandrogomezmba@gmail.com>
2021-02-01 20:50:16 -0600

    finding the winner

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -25,13 +25,18 @@ with open(file_path) as csvfile:
                 got_votes[c] = 1
     # print('Khan', got_votes['Khan'])
     print(got_votes)
-
+winner = 0
+for key, value in got_votes.items():
+    if winner < value:
+        winner = value
+        elected = key
+print(elected)
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {total_votes}")
 print("-------------------------")
 print(
-    f"Khan: {round(float(got_votes['Khan']/total_votes*100),5)}% ({got_votes['Khan']})")
+    f"Khan: {str(round(float(got_votes['Khan']/total_votes*100),5))}% ({got_votes['Khan']})")
 print(
     f"Correy: {round(float(got_votes['Correy']/total_votes*100),5)}% ({got_votes['Correy']})")
 print(
@@ -39,5 +44,5 @@ print(
 # print(
 #     f"OTooley: {round(float(got_votes['OTooley']/total_votes*100),5)}% ({got_votes['OTooley']})")
 print("-------------------------")
-print("Winner: Khan")
+print("Winner: ")
 print("-------------------------")
