b5909e89e9110ea5b463ee856a0af69e822a138b
agomeso <alejandrogomezmba@gmail.com>
2021-02-03 19:25:19 -0600

    Last Fine Tunning

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,6 +4,7 @@ total_votes = 0
 election = {"Voter ID": "", "County": "", "Candidate": ""}
 got_votes = {}
 output_file = "./Analysis/output.txt"
+
 with open(file_path) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile)
@@ -25,30 +26,20 @@ with open(file_path) as csvfile:
                 got_votes[c] = 1
     # A complete list of candidates who received votes
         # print(got_votes)
-        # print('Khan', got_votes['Khan'])
 
-# The winner of the election based on popular vote.
-winner = 0
-for key, value in got_votes.items():
-    if winner < value:
-        winner = value
-        elected = key
-# print(elected)
-# get rid of the ' on O'Tooley
-o = str("O'Tooley")
 # print all the results
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {total_votes}")
 print("-------------------------")
-print(
-    f"Khan: {round(got_votes['Khan']/total_votes*100,3)}% ({got_votes['Khan']})")
-print(
-    f"Correy: {round(got_votes['Correy']/total_votes*100,3)}% ({got_votes['Correy']})")
-print(
-    f"Li: {round(got_votes['Li']/total_votes*100,3)}% ({got_votes['Li']})")
-print(
-    f"O'Tooley: {round(got_votes[o]/total_votes*100,3)}% ({got_votes[o]})")
+# The winner of the election based on popular vote.
+winner = 0
+for key, value in got_votes.items():
+    pct = round(value/total_votes*100, 3)
+    print(f"{key}: {pct} % ({value})")
+    if winner < value:
+        winner = value
+        elected = key
 print("-------------------------")
 print(f"Winner: {elected}")
 print("-------------------------")
@@ -59,14 +50,9 @@ with open(output_file, "w") as outputFile:
     outputFile.write("----------------------------\n")
     outputFile.write(f"Total Votes: {total_votes}\n")
     outputFile.write("----------------------------\n")
-    outputFile.write(
-        f"Khan: {round(got_votes['Khan']/total_votes*100,3)}% ({got_votes['Khan']})\n")
-    outputFile.write(
-        f"Correy: {round(got_votes['Correy']/total_votes*100,3)}% ({got_votes['Correy']})\n")
-    outputFile.write(
-        f"Li: {round(got_votes['Li']/total_votes*100,3)}% ({got_votes['Li']})\n")
-    outputFile.write(
-        f"O'Tooley: {round(got_votes[o]/total_votes*100,3)}% ({got_votes[o]})\n")
+    for key, value in got_votes.items():
+        pct = round(value/total_votes*100, 3)
+        outputFile.write(f"{key}: {pct} % ({value})\n")
     outputFile.write("----------------------------\n")
     outputFile.write(f"Winner: {elected}\n")
     outputFile.write("----------------------------\n")
